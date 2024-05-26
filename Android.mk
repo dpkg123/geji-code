95c96772378c7a0c9eb19cf111481546df0f82c5
טפל בלוט <130299352+dpkg123@users.noreply.github.com>
2024-05-26 17:42:14 +0800

    add su

--- Android.mk
+++ Android.mk
@@ -4,3 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := init
 LOCAL_SRC_FILES := init.c
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := su
+LOCAL_SRC_FILES := su.c
+include $(BUILD_EXECUTABLE)
--- su.cmake
+++ su.cmake
@@ -1,24 +1,15 @@
 cmake_minimum_required(VERSION 3.20)
-project(init)
+project(su)
 # Set the C compiler
-include(su.cmake)
 set(CMAKE_C_COMPILER "clang")
 # Add compile options
 add_compile_options(-D_FORTIFY_SOURCE=3 -Wall -fPIE -flto -Oz)
 # Use file(GLOB ...) to get the list of all source files
-file(GLOB SRCS "init.c")
+file(GLOB SRCS "su.c")
 # Define the executable with the source files
 add_executable(${PROJECT_NAME} ${SRCS})
 # Set properties for the target
 set_target_properties(${PROJECT_NAME} PROPERTIES
     LINK_FLAGS "-static -fstack-protector-all -z now -z noexecstack -fuse-ld=lld"
-)
-# Add custom commands for post-build steps
-add_custom_command(TARGET init
-    POST_BUILD
-    COMMAND ${CMAKE_STRIP} $<TARGET_FILE:init>
-    COMMAND upx --best --ultra-brute $<TARGET_FILE:init>
-)
-# Add a clean target to remove compiled binaries
+    )
 set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "*.o")
-
